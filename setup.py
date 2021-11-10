57b347d286fbe8b447a5de6329e1b7b27a7532cb
Sébastien Weber <seba.weber@gmail.com>
2021-11-10 21:51:34 +0100

    src and MIT license

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
 import toml
-from pymodaq_plugins_physik_instrumente import version
 
 config = toml.load('./plugin_info.toml')
+SHORT_PLUGIN_NAME = config['plugin-info']['SHORT_PLUGIN_NAME']
 PLUGIN_NAME = f"pymodaq_plugins_{config['plugin-info']['SHORT_PLUGIN_NAME']}"
+
+
+from pathlib import Path
+
+with open(str(Path(__file__).parent.joinpath(f'src/{PLUGIN_NAME}/VERSION')), 'r') as fvers:
+    version = fvers.read().strip()
+
 
 with open('README.rst') as fd:
     long_description = fd.read()
@@ -23,7 +30,7 @@ setupOpts = dict(
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Human Machine Interfaces",
         "Topic :: Scientific/Engineering :: Visualization",
-        "License :: CeCILL-B Free Software License Agreement (CECILL-B)",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Software Development :: User Interfaces",
@@ -31,14 +38,12 @@ setupOpts = dict(
 
 
 setup(
-    version=version.get_version(),
-    packages=find_packages(),
-    package_data={'': ['*.dll']},
+    version=version,
+    packages=find_packages(where='./src'),
+    package_dir={'': 'src'},
     include_package_data=True,
-    entry_points={'pymodaq.plugins': f'default = {PLUGIN_NAME}'},
-    install_requires=['toml',
-        'pymodaq>=2.0',
-        ]+config['plugin-install']['packages-required'],
+    entry_points={'pymodaq.plugins': f'{SHORT_PLUGIN_NAME} = {PLUGIN_NAME}'},
+    install_requires=['toml', ]+config['plugin-install']['packages-required'],
     **setupOpts
 )
 
